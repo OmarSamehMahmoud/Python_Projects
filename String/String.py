240826e2beab6305f17b7458b26c232c1c6f3f29
Omar Sameh Mahmoud <62811657+OmarSamehMahmoud@users.noreply.github.com>
2021-09-03 13:22:02 +0200

    Operations on String
    Operations on String

---
+++ String/String.py
@@ -0,0 +1,10 @@
+string="omar"
+print(string[0])
+print(string[0:2])
+print(string[:2])
+print(string[1:])
+string=string.upper()
+print(string)
+string=string.lower()
+print(string)
+print(len(string))
